5b15214aca1adfefa68d76db7b621c7eec747e97
Jonathan Behrens <jbehrens@microsoft.com>
2023-02-14 11:45:05 -0800

    Run 'make generate' and 'make mocks'
    Signed-off-by: Jonathan Behrens <jbehrens@microsoft.com>

--- api/v1alpha1/groupversion_info.go
+++ api/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the bmc v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=bmc.tinkerbell.org
+// +kubebuilder:object:generate=true
+// +groupName=bmc.tinkerbell.org
 package v1alpha1
 
 import (
